3b37eea3a8dab0030f1cf50a38cb0dfed2a9cf5b
Michael Kyulu <michaelkyulu4@gmail.com>
2026-03-21 15:14:42 +0300

    Created a complete system with a proper UI, navigation bars, debugged

--- goats/views.py
+++ goats/views.py
@@ -1,9 +1,46 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Goat
+from .models import Goat, DeathRecord, SaleRecord
 from .forms import GoatForm
 from .forms import DeathRecordForm, SaleRecordForm
+from django.db.models import Sum
 
+def dashboard(request):
 
+    total_goats = Goat.objects.count()
+
+    alive_goats = Goat.objects.filter(is_alive=True, is_sold=False).count()
+
+    sold_goats = Goat.objects.filter(is_sold=True).count()
+
+    dead_goats = Goat.objects.filter(is_alive=False, is_sold=False).count()
+
+    total_purchase = Goat.objects.aggregate(
+        Sum('purchase_price')
+    )['purchase_price__sum'] or 0
+
+    total_sales = SaleRecord.objects.aggregate(
+        Sum('sale_price')
+    )['sale_price__sum'] or 0
+
+    profit = total_sales - total_purchase
+
+    if sold_goats > 0:
+        avg_profit_per_goat = profit / sold_goats
+    else:
+        avg_profit_per_goat = 0
+
+    context = {
+        "total_goats": total_goats,
+        "alive_goats": alive_goats,
+        "sold_goats": sold_goats,
+        "dead_goats": dead_goats,
+        "total_purchase": total_purchase,
+        "total_sales": total_sales,
+        "profit": profit,
+        "avg_profit_per_goat": avg_profit_per_goat
+    }
+
+    return render(request, "goats/dashboard.html", context)
 def goat_list(request):
     goats = Goat.objects.all()
     return render(request, 'goats/goat_list.html', {'goats': goats})
@@ -63,6 +100,9 @@ def record_death(request, pk):
 def record_sale(request, pk):
 
     goat = get_object_or_404(Goat, pk=pk)
+
+    if goat.is_sold:
+        return redirect('goat_detail', pk=goat.id)
 
     if request.method == "POST":
 
