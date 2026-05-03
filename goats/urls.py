d095f9d106b15fec1768cc9383055c4201518ce0
Michael Kyulu <michaelkyulu4@gmail.com>
2026-05-03 10:02:55 +0300

    Fixing url, duplicate url

--- goats/urls.py
+++ goats/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.goat_list, name='goat_list'),
+    path('', views.dashboard, name='dashboard'),  
+    path('goats/', views.goat_list, name='goat_list'),
     path('add/', views.add_goat, name='add_goat'),
     path('<int:pk>/', views.goat_detail, name='goat_detail'),
     path('<int:pk>/death/', views.record_death, name='record_death'),
     path('<int:pk>/sale/', views.record_sale, name='record_sale'),
-    path('', views.dashboard, name='dashboard'),
-],
+]
